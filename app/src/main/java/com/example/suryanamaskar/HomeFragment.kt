a4906e896f4e9a1ede17b78603b3d16fc630b442
Abinash6000 <abinashdash6000@gmail.com>
2023-07-07 21:51:15 +0530

    Implemented StartFragment

--- app/src/main/java/com/example/suryanamaskar/HomeFragment.kt
+++ app/src/main/java/com/example/suryanamaskar/HomeFragment.kt
@@ -5,12 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.suryanamaskar.databinding.FragmentHomeBinding
+
 class HomeFragment : Fragment() {
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        binding.btnStart.setOnClickListener {
+            val navController = findNavController()
+            navController.navigate(R.id.action_homeFragment_to_startFragment)
+        }
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
--- app/src/main/java/com/example/suryanamaskar/StartFragment.kt
+++ app/src/main/java/com/example/suryanamaskar/StartFragment.kt
@@ -1,16 +1,169 @@
 package com.example.suryanamaskar
 
+import android.content.res.Resources
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.os.SystemClock
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.navigation.fragment.findNavController
+import com.example.suryanamaskar.databinding.FragmentStartBinding
+import java.util.Locale
+import java.util.Timer
+import java.util.TimerTask
+
 class StartFragment : Fragment() {
+    private var _binding: FragmentStartBinding? = null
+    private val binding get() = _binding!!
+
+    private var running = false
+    var rounds = 3
+    var repetitions = 2
+    var total = 0
+    val startTimeInMillis = 5000L
+    val timePerPose = startTimeInMillis/12
+    var timeLeftInMillis = startTimeInMillis
+    var startCountDownTimer: CountDownTimer? = null
+
+    val mantras = listOf(
+        "Om Mitraya Namaha",
+        "Om Ravaye Namaha",
+        "Om Suryaya Namaha",
+        "Om Bhanave Namaha",
+        "Om Khagaya Namaha",
+        "Om Pushne Namaha",
+        "Om Hiranya Garbhaye Namaha",
+        "Om Marichaye Namaha",
+        "Om Adityaya Namaha",
+        "Om Savitre Namaha",
+        "Om Arkaya Namaha",
+        "Om Bhaskaraya Namaha"
+    )
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_start, container, false)
+        _binding = FragmentStartBinding.inflate(inflater, container, false)
+
+        rounds = 3
+        startTimer()
+
+        binding.pauseBtn.setOnClickListener {
+            if (running) {
+                pauseTimer();
+            } else {
+                startTimer();
+            }
+        }
+
+        return binding.root
+    }
+
+    private fun pauseTimer() {
+        startCountDownTimer?.cancel()
+        running = false
+        binding.pauseBtn.text = "Resume"
+    }
+
+    private fun updateCountDownText() {
+        val minutes = (timeLeftInMillis / 1000) / 60
+        val seconds = (timeLeftInMillis / 1000) % 60
+        val timeLeftFormatted = String.format(Locale.getDefault(),"%02d:%02d", minutes, seconds)
+        binding.tvTime.text = timeLeftFormatted
+    }
+
+    private fun startTimer() {
+        if(!running) {
+            startCountDownTimer = object : CountDownTimer(timeLeftInMillis, 1000L) {
+                override fun onTick(millisUntilFinished: Long) {
+                    timeLeftInMillis = millisUntilFinished
+                    updateCountDownText()
+                    updatePoseImage()
+                }
+
+                override fun onFinish() {
+                    if(--rounds != 0) {
+                        timeLeftInMillis = startTimeInMillis
+                        startCountDownTimer = null
+                        running = false
+                        total++
+                        startTimer()
+                    } else {
+                        repetitions--
+                        running = false
+                        // navigate to break fragment
+                        findNavController().navigate(R.id.action_startFragment_to_breakFragment)
+                    }
+                }
+
+            }.start()
+
+            running = true
+            binding.pauseBtn.text = "Pause"
+        }
+    }
+
+    private fun updatePoseImage() {
+        when {
+            timeLeftInMillis >= (11 * timePerPose) -> {
+                binding.ivPose.setImageDrawable(ContextCompat.getDrawable(requireContext(),R.drawable.pose_1_f))
+                binding.tvMantra.text = mantras[0]
+            }
+            timeLeftInMillis >= (10 * timePerPose) -> {
+                binding.ivPose.setImageDrawable(ContextCompat.getDrawable(requireContext(),R.drawable.pose_2_f))
+                binding.tvMantra.text = mantras[1]
+            }
+            timeLeftInMillis >= (9 * timePerPose) -> {
+                binding.ivPose.setImageDrawable(ContextCompat.getDrawable(requireContext(),R.drawable.pose_3_f))
+                binding.tvMantra.text = mantras[2]
+            }
+            timeLeftInMillis >= (8 * timePerPose) -> {
+                binding.ivPose.setImageDrawable(ContextCompat.getDrawable(requireContext(),R.drawable.pose_4_f))
+                binding.tvMantra.text = mantras[3]
+            }
+            timeLeftInMillis >= (7 * timePerPose) -> {
+                binding.ivPose.setImageDrawable(ContextCompat.getDrawable(requireContext(),R.drawable.pose_5_f))
+                binding.tvMantra.text = mantras[4]
+            }
+            timeLeftInMillis >= (6 * timePerPose) -> {
+                binding.ivPose.setImageDrawable(ContextCompat.getDrawable(requireContext(),R.drawable.pose_6_f))
+                binding.tvMantra.text = mantras[5]
+            }
+            timeLeftInMillis >= (5 * timePerPose) -> {
+                binding.ivPose.setImageDrawable(ContextCompat.getDrawable(requireContext(),R.drawable.pose_7_f))
+                binding.tvMantra.text = mantras[6]
+            }
+            timeLeftInMillis >= (4 * timePerPose) -> {
+                binding.ivPose.setImageDrawable(ContextCompat.getDrawable(requireContext(),R.drawable.pose_5_f))
+                binding.tvMantra.text = mantras[7]
+            }
+            timeLeftInMillis >= (3 * timePerPose) -> {
+                binding.ivPose.setImageDrawable(ContextCompat.getDrawable(requireContext(),R.drawable.pose_4_f))
+                binding.tvMantra.text = mantras[8]
+            }
+            timeLeftInMillis >= (2 * timePerPose) -> {
+                binding.ivPose.setImageDrawable(ContextCompat.getDrawable(requireContext(),R.drawable.pose_3_f))
+                binding.tvMantra.text = mantras[9]
+            }
+            timeLeftInMillis >= (1 * timePerPose) -> {
+                binding.ivPose.setImageDrawable(ContextCompat.getDrawable(requireContext(),R.drawable.pose_2_f))
+                binding.tvMantra.text = mantras[10]
+            }
+            timeLeftInMillis >= (0 * timePerPose) -> {
+                binding.ivPose.setImageDrawable(ContextCompat.getDrawable(requireContext(),R.drawable.pose_1_f))
+                binding.tvMantra.text = mantras[11]
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
