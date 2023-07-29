b19d8ecfa419fcebff3157780b5e179b872701e9
Abinash6000 <abinashdash6000@gmail.com>
2023-07-29 15:49:52 +0530

    simpler done and home fragment

--- app/src/main/java/com/example/suryanamaskar/fragment/DoneFragment.kt
+++ app/src/main/java/com/example/suryanamaskar/fragment/DoneFragment.kt
@@ -5,14 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.suryanamaskar.R
+import com.example.suryanamaskar.databinding.FragmentDoneBinding
 
 class DoneFragment : Fragment() {
+    private var _binding: FragmentDoneBinding? = null
+    private val binding get() = _binding!!
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_done, container, false)
+        _binding = FragmentDoneBinding.inflate(inflater, container, false)
+
+        binding.btnHome.setOnClickListener {
+            findNavController().navigate(R.id.action_doneFragment_to_homeFragment2)
+        }
+
+        return binding.root
     }
 }
