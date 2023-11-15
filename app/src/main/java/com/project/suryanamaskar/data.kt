9b81290f898e67fcbbf533d35f9c37c21c64b8f8
Abinash6000 <abinashdash6000@gmail.com>
2023-11-16 01:54:52 +0530

    skip button and humans button implemented and back navigation crash handled

--- app/src/main/java/com/project/suryanamaskar/data.kt
+++ app/src/main/java/com/project/suryanamaskar/data.kt
@@ -14,3 +14,5 @@ var repetitionsCount: Int = 0
 
 var currRounds = roundsCount
 var currRepetitions = repetitionsCount
+
+var humans = false
--- app/src/main/java/com/project/suryanamaskar/fragment/StartFragment.kt
+++ app/src/main/java/com/project/suryanamaskar/fragment/StartFragment.kt
@@ -2,21 +2,24 @@ package com.project.suryanamaskar.fragment
 
 import android.os.Bundle
 import android.os.CountDownTimer
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.addCallback
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.suryanamaskar.R
+import com.example.suryanamaskar.databinding.FragmentStartBinding
 import com.project.suryanamaskar.currRepetitions
 import com.project.suryanamaskar.currRounds
-import com.example.suryanamaskar.databinding.FragmentStartBinding
+import com.project.suryanamaskar.humans
 import com.project.suryanamaskar.roundsCount
 import java.util.Locale
 
+
 class StartFragment : Fragment() {
+
     private var _binding: FragmentStartBinding? = null
     private val binding get() = _binding!!
 
@@ -42,6 +45,18 @@ class StartFragment : Fragment() {
         "Om Bhaskaraya Namaha"
     )
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val callback = requireActivity().onBackPressedDispatcher.addCallback(this) {
+            // Handle the back button event
+            startCountDownTimer!!.cancel()
+            findNavController().navigate(R.id.action_startFragment_to_homeFragment)
+        }
+
+        callback.isEnabled = true
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -59,6 +74,33 @@ class StartFragment : Fragment() {
             }
         }
 
+        binding.humans.setOnClickListener {
+            humans = !humans
+        }
+
+        binding.skipBtn.setOnClickListener {
+            // cancel the timer
+            startCountDownTimer!!.cancel()
+            timeLeftInMillis = 1000L
+            running = false
+
+            // execute the same code you would execute when the timer finishes
+            if(--currRounds > 0) {
+                timeLeftInMillis = startTimeInMillis
+                startCountDownTimer = null
+                running = false
+                total++
+                startTimer()
+            } else {
+                currRepetitions--
+                running = false
+                if(currRepetitions > 0)
+                    findNavController().navigate(R.id.action_startFragment_to_breakFragment)
+                else
+                    findNavController().navigate(R.id.action_startFragment_to_doneFragment)
+            }
+        }
+
         return binding.root
     }
 
@@ -66,6 +108,8 @@ class StartFragment : Fragment() {
         startCountDownTimer?.cancel()
         running = false
         binding.pauseBtn.text = "Resume"
+        val drawable = ContextCompat.getDrawable(requireContext(), R.drawable.round_play_arrow_24)
+        binding.pauseBtn.setCompoundDrawablesWithIntrinsicBounds(null, null, drawable, null)
     }
 
     private fun updateCountDownText() {
@@ -86,7 +130,6 @@ class StartFragment : Fragment() {
 
                 override fun onFinish() {
                     if(--currRounds > 0) {
-                        Log.d("adfasdfasdf", currRounds.toString())
                         timeLeftInMillis = startTimeInMillis
                         startCountDownTimer = null
                         running = false
@@ -106,81 +149,143 @@ class StartFragment : Fragment() {
 
             running = true
             binding.pauseBtn.text = "Pause"
+            val drawable = ContextCompat.getDrawable(requireContext(), R.drawable.round_pause_24)
+            binding.pauseBtn.setCompoundDrawablesWithIntrinsicBounds(null, null, drawable, null)
         }
     }
 
     private fun updatePoseImage() {
         when {
             timeLeftInMillis >= (11 * timePerPose) -> {
-                binding.ivPose.setImageDrawable(ContextCompat.getDrawable(requireContext(),
-                    R.drawable.pose_1_f
-                ))
+                if(humans)
+                    binding.ivPose.setImageDrawable(ContextCompat.getDrawable(requireContext(),
+                        R.drawable.pose_1_12_f
+                    ))
+                else
+                    binding.ivPose.setImageDrawable(ContextCompat.getDrawable(requireContext(),
+                        R.drawable.pose_1_12
+                    ))
                 binding.tvMantra.text = mantras[0]
             }
             timeLeftInMillis >= (10 * timePerPose) -> {
-                binding.ivPose.setImageDrawable(ContextCompat.getDrawable(requireContext(),
-                    R.drawable.pose_2_f
-                ))
+                if(humans)
+                    binding.ivPose.setImageDrawable(ContextCompat.getDrawable(requireContext(),
+                        R.drawable.pose_2_11_f
+                    ))
+                else
+                    binding.ivPose.setImageDrawable(ContextCompat.getDrawable(requireContext(),
+                        R.drawable.pose_2_11
+                    ))
                 binding.tvMantra.text = mantras[1]
             }
             timeLeftInMillis >= (9 * timePerPose) -> {
-                binding.ivPose.setImageDrawable(ContextCompat.getDrawable(requireContext(),
-                    R.drawable.pose_3_f
-                ))
+                if(humans)
+                    binding.ivPose.setImageDrawable(ContextCompat.getDrawable(requireContext(),
+                        R.drawable.pose_3_10_f
+                    ))
+                else
+                    binding.ivPose.setImageDrawable(ContextCompat.getDrawable(requireContext(),
+                        R.drawable.pose_3_10
+                    ))
                 binding.tvMantra.text = mantras[2]
             }
             timeLeftInMillis >= (8 * timePerPose) -> {
-                binding.ivPose.setImageDrawable(ContextCompat.getDrawable(requireContext(),
-                    R.drawable.pose_4_f
-                ))
+                if(humans)
+                    binding.ivPose.setImageDrawable(ContextCompat.getDrawable(requireContext(),
+                        R.drawable.pose_4_9_f
+                    ))
+                else
+                    binding.ivPose.setImageDrawable(ContextCompat.getDrawable(requireContext(),
+                        R.drawable.pose_4_9
+                    ))
                 binding.tvMantra.text = mantras[3]
             }
             timeLeftInMillis >= (7 * timePerPose) -> {
-                binding.ivPose.setImageDrawable(ContextCompat.getDrawable(requireContext(),
-                    R.drawable.pose_5_f
-                ))
+                if(humans)
+                    binding.ivPose.setImageDrawable(ContextCompat.getDrawable(requireContext(),
+                        R.drawable.pose_5_8_f
+                    ))
+                else
+                    binding.ivPose.setImageDrawable(ContextCompat.getDrawable(requireContext(),
+                        R.drawable.pose_5_8
+                    ))
                 binding.tvMantra.text = mantras[4]
             }
             timeLeftInMillis >= (6 * timePerPose) -> {
-                binding.ivPose.setImageDrawable(ContextCompat.getDrawable(requireContext(),
-                    R.drawable.pose_6_f
-                ))
+                if(humans)
+                    binding.ivPose.setImageDrawable(ContextCompat.getDrawable(requireContext(),
+                        R.drawable.pose_6_f
+                    ))
+                else
+                    binding.ivPose.setImageDrawable(ContextCompat.getDrawable(requireContext(),
+                        R.drawable.pose_6
+                    ))
                 binding.tvMantra.text = mantras[5]
             }
             timeLeftInMillis >= (5 * timePerPose) -> {
-                binding.ivPose.setImageDrawable(ContextCompat.getDrawable(requireContext(),
-                    R.drawable.pose_7_f
-                ))
+                if(humans)
+                    binding.ivPose.setImageDrawable(ContextCompat.getDrawable(requireContext(),
+                        R.drawable.pose_7_f
+                    ))
+                else
+                    binding.ivPose.setImageDrawable(ContextCompat.getDrawable(requireContext(),
+                        R.drawable.pose_7
+                    ))
                 binding.tvMantra.text = mantras[6]
             }
             timeLeftInMillis >= (4 * timePerPose) -> {
-                binding.ivPose.setImageDrawable(ContextCompat.getDrawable(requireContext(),
-                    R.drawable.pose_5_f
-                ))
+                if(humans)
+                    binding.ivPose.setImageDrawable(ContextCompat.getDrawable(requireContext(),
+                        R.drawable.pose_5_8_f
+                    ))
+                else
+                    binding.ivPose.setImageDrawable(ContextCompat.getDrawable(requireContext(),
+                        R.drawable.pose_5_8
+                    ))
                 binding.tvMantra.text = mantras[7]
             }
             timeLeftInMillis >= (3 * timePerPose) -> {
-                binding.ivPose.setImageDrawable(ContextCompat.getDrawable(requireContext(),
-                    R.drawable.pose_4_f
-                ))
+                if(humans)
+                    binding.ivPose.setImageDrawable(ContextCompat.getDrawable(requireContext(),
+                        R.drawable.pose_4_9_f
+                    ))
+                else
+                    binding.ivPose.setImageDrawable(ContextCompat.getDrawable(requireContext(),
+                        R.drawable.pose_4_9
+                    ))
                 binding.tvMantra.text = mantras[8]
             }
             timeLeftInMillis >= (2 * timePerPose) -> {
-                binding.ivPose.setImageDrawable(ContextCompat.getDrawable(requireContext(),
-                    R.drawable.pose_3_f
-                ))
+                if(humans)
+                    binding.ivPose.setImageDrawable(ContextCompat.getDrawable(requireContext(),
+                        R.drawable.pose_3_10_f
+                    ))
+                else
+                    binding.ivPose.setImageDrawable(ContextCompat.getDrawable(requireContext(),
+                        R.drawable.pose_3_10
+                    ))
                 binding.tvMantra.text = mantras[9]
             }
             timeLeftInMillis >= (1 * timePerPose) -> {
-                binding.ivPose.setImageDrawable(ContextCompat.getDrawable(requireContext(),
-                    R.drawable.pose_2_f
-                ))
+                if(humans)
+                    binding.ivPose.setImageDrawable(ContextCompat.getDrawable(requireContext(),
+                        R.drawable.pose_2_11_f
+                    ))
+                else
+                    binding.ivPose.setImageDrawable(ContextCompat.getDrawable(requireContext(),
+                        R.drawable.pose_2_11
+                    ))
                 binding.tvMantra.text = mantras[10]
             }
             timeLeftInMillis >= (0 * timePerPose) -> {
-                binding.ivPose.setImageDrawable(ContextCompat.getDrawable(requireContext(),
-                    R.drawable.pose_1_f
-                ))
+                if(humans)
+                    binding.ivPose.setImageDrawable(ContextCompat.getDrawable(requireContext(),
+                        R.drawable.pose_1_12_f
+                    ))
+                else
+                    binding.ivPose.setImageDrawable(ContextCompat.getDrawable(requireContext(),
+                        R.drawable.pose_1_12
+                    ))
                 binding.tvMantra.text = mantras[11]
             }
         }
